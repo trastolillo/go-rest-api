34f785d06a123d6e27896a6a631f085d0d867187
trastolillo <trastolillo@tutanota.com>
2021-08-06 04:24:27 +0200

    inicio

---
+++ pkg/server/api.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type api struct {
+	router http.Handler
+}
+
+type Server interface {
+	Router() http.Handler
+}
+
+func New() Server {
+	a := &api{}
+	r := mux.NewRouter()
+	a.router = r
+	return a
+}
+
+func (a *api) Router() http.Handler {
+	return a.router
+}
